af81e3d14397ab5f4fd9899e7f97a661c7b77662
zarkio42 <pedro.contato24@gmail.com>
2024-01-20 22:43:10 -0300

    darkmode

--- Projeto Starbucks/Projeto Starbucks/index.js
+++ Projeto Starbucks/Projeto Starbucks/index.js
@@ -4,23 +4,56 @@ const miniaturaVerde = document.getElementById("minVerde");
 const miniaturaLaranja = document.getElementById("minLaranja");
 const circulo = document.getElementById("circulo");
 
-miniaturaRosa.addEventListener("click", () =>{
-    imgStarbucks.src = "./images/img3.png";
-    circulo.style.backgroundColor = "#bf3899"
 
+miniaturaRosa.addEventListener("click", () =>{
+    let page = document.body
+    if(page.className.includes("dark")){
+        circulo.style.backgroundColor = "#781b5c"
+        imgStarbucks.src = "./images/img3.png";
+    }
+    else{
+        imgStarbucks.src = "./images/img3.png";
+        circulo.style.backgroundColor = "#bf3899"
+    }
 })
 
 miniaturaLaranja.addEventListener("click", () =>{
-    imgStarbucks.src = "./images/img2.png";
-    circulo.style.backgroundColor = "#e9acb5"
-    imgStarbucks.style.BoxShadow ="0px 0px 5px rgba(50, 50, 50, 0.77)"
+    let page = document.body
+    if(page.className.includes("dark")){
+        imgStarbucks.src = "./images/img2.png";
+        circulo.style.backgroundColor = "#b05e64"
+    }
+    else{
+        imgStarbucks.src = "./images/img2.png";
+        circulo.style.backgroundColor = "#e9acb5"
+    }
 })
 
 miniaturaVerde.addEventListener("click", () =>{
-    imgStarbucks.src = "./images/img1.png"
-    circulo.style.backgroundColor = "#017143"
+    let page = document.body
+    if(page.className.includes("dark")){
+        circulo.style.backgroundColor = "#00472b"
+        imgStarbucks.src = "./images/img1.png"
+    }
+    else{
+        imgStarbucks.src = "./images/img1.png"
+        circulo.style.backgroundColor = "#017143"
+    }
 })
 
 function DarkMode(){
-    
+    let iconLight = document.getElementById("iconLight");
+    let page = document.body
+
+    if(!iconLight.className.includes("idark"))
+    {
+        iconLight.classList.add("idark");
+        page.classList.add("dark");
+    } 
+    else
+    {
+        iconLight.classList.remove("idark");
+        page.classList.remove("dark");
+    }
+
 }
